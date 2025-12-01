564877c6bbbc4aec3e12915f5f969b9a0466dbae
Tegar Aditya <tegaraditya53@gmail.com>
2025-12-01 15:17:29 +0700

    refactor: update avatar endpoint to use query parameters and generate random name with nanoid

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,52 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+import { secureHeaders } from "hono/secure-headers";
 import { renderToStaticMarkup } from "react-dom/server";
 import { sValidator } from "@hono/standard-validator";
 import { type } from "arktype";
+import { nanoid } from 'nanoid';
 import Avatar from "boring-avatars";
 import React from "react";
 
 const PORT = Number(process.env.PORT ?? 3000);
 
 const schema = {
-  params: type({
-    name: "string",
-  }),
   query: type({
-    size: "(string.integer.parse)?",
-    variant: "('marble' | 'beam' | 'pixel' | 'sunset' | 'ring' | 'bauhaus')?",
+    "name?": "string",
+    "size?": "string.integer.parse",
+    "variant?": "'marble' | 'beam' | 'pixel' | 'sunset' | 'ring' | 'bauhaus'",
+    "colors?": type("string").pipe((val: string) =>
+      val.split(",").map((s) => {
+        const t = s.trim();
+        return t.startsWith("#") ? t : `#${t}`;
+      })
+    ),
+    "square?": type("'true' | 'false'").pipe((val) => val === "true"),
   }),
 };
 
 const app = new Hono();
 
-app.get(
-  "/avatar/:name",
-  sValidator("param", schema.params),
-  sValidator("query", schema.query),
-  (c) => {
-    const { name } = c.req.valid("param");
-    const { size = 40, variant = "beam" } = c.req.valid("query");
+app.use(cors()).use(logger()).use(secureHeaders());
 
-    const svg = renderToStaticMarkup(
-      React.createElement(Avatar, {
-        size,
-        name,
-        variant,
-      })
-    );
+app.get("/", sValidator("query", schema.query), (c) => {
+  const { name, size, variant, colors, square } = c.req.valid("query");
 
-    return c.body(svg, 200, {
-      "Content-Type": "image/svg+xml",
-    });
-  }
-);
+  const svg = renderToStaticMarkup(
+    React.createElement(Avatar, {
+      size,
+      name: name ?? nanoid(),
+      variant,
+      colors,
+      square,
+    })
+  );
+
+  return c.body(svg, 200, {
+    "Content-Type": "image/svg+xml",
+  });
+});
 
 export default {
   port: PORT,
